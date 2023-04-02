7aedf0797c0364a9919c161b789bad2333212d02
James Alhassan <jamesalhassan052@gmail.com>
2023-04-02 22:38:50 +0000

    add preloader js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ const links = document.getElementById("nav-links");
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    preloader.classList.add("hide-preloader");
+  }, 3000);
+});
